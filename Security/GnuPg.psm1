0e1bf9c148fc7cb5441dd7ef5d37e569ed4502d3
Nigel Ainscoe <nigel.ainscoe@gmail.com>
2018-07-27 12:23:15 +0100

    Update to use latest version of Gpg4Win

--- Security/GnuPg.psm1
+++ Security/GnuPg.psm1
@@ -32,7 +32,7 @@
 		
 		[Parameter()]
 		[ValidateNotNullOrEmpty()]
-		[string]$DownloadUrl = 'http://files.gpg4win.org/gpg4win-2.2.5.exe'
+		[string]$DownloadUrl = 'http://files.gpg4win.org/gpg4win-3.1.2.exe'
 		
 	)
 	process {
